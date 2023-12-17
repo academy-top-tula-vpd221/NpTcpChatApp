0e092fba0ba6884ac14fce915398bc232b75e9aa
maxefimov <efimov.top.academy@gmail.com>
2023-12-17 17:57:22 +0300

    Добавьте файлы проекта.

---
+++ NpTcpChatListenerApp/ClientObject.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NpTcpChatListenerApp
+{
+    internal class ClientObject
+    {
+        internal string Id { get; } = Guid.NewGuid().ToString();
+
+        internal StreamWriter Writer { get; }
+        internal StreamReader Reader { get; }
+
+        TcpClient client;
+        ListenerObject listener;
+
+        public ClientObject(TcpClient client, ListenerObject listener)
+        {
+            this.client = client; 
+            this.listener = listener;
+
+            var stream = client.GetStream();
+
+            Writer = new StreamWriter(stream);
+            Reader = new StreamReader(stream);
+        }
+
+        internal async Task ProcessAsync()
+        {
+            try
+            {
+                string? clientName = await Reader.ReadLineAsync();
+                string? message = $"{clientName} commin to chat";
+                await listener.SendMessageAsync(message, this.Id);
+                Console.WriteLine(message);
+
+                while (true)
+                {
+                    try
+                    {
+                        message = await Reader.ReadLineAsync();
+                        if (message is null) continue;
+                        message = $"{clientName}: {message}";
+                        Console.WriteLine(message);
+                        await listener.SendMessageAsync(message, this.Id);
+                    }
+                    catch
+                    {
+                        message = $"{clientName} out from chat";
+                        Console.WriteLine(message);
+                        await listener.SendMessageAsync(message, this.Id);
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                listener.RemoveClient(this.Id);
+            }
+        }
+
+        internal void Close()
+        {
+            Writer.Close();
+            Reader.Close();
+            client.Close();
+        }
+    }
+}
---
+++ NpTcpChatListenerApp/ListenerObject.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Security;
+
+namespace NpTcpChatListenerApp
+{
+    class ListenerObject
+    {
+        TcpListener listener;
+        List<ClientObject> clients;
+
+        public ListenerObject()
+        {
+            listener = new TcpListener(IPAddress.Loopback, 5000);
+            clients = new List<ClientObject>();
+        }
+
+        internal void RemoveClient(string id)
+        {
+            ClientObject? client = clients.FirstOrDefault(c => c.Id == id);
+            if (client is not null)
+                clients.Remove(client);
+            client.Close();
+        }
+
+        internal async Task ListenAsync()
+        {
+            try
+            {
+                listener.Start();
+                Console.WriteLine("Server chat start. Waitng connections...");
+
+                TcpClient clientTcp = await listener.AcceptTcpClientAsync();
+                ClientObject client = new ClientObject(clientTcp, this);
+
+                clients.Add(client);
+                await Task.Run(client.ProcessAsync);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+
+        internal async Task SendMessageAsync(string id, string message)
+        {
+            foreach(var client in clients)
+            {
+                if(client.Id != id)
+                {
+                    await client.Writer.WriteLineAsync(message);
+                    await client.Writer.FlushAsync();
+                }
+            }
+        }
+
+        internal void Disconnect()
+        {
+            foreach (var client in clients)
+                client.Close();
+
+            listener.Stop();
+        }
+    }
+}
